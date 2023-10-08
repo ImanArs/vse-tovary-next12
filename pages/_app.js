b6629ac5f45fc8ca1e3bead135bd8afc7e9ffc35
ImanArs <imanbaevemaill.ru@gmail.com>
2023-10-08 14:12:08 +0600

    add: meta

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,30 @@ import { store } from '../features/store'
 import Header from '../components/header/Header'
 import Footer from '../components/footer/Footer'
 import CategoryRoutes from '../features/slices/categoryRotes'
+import Head from 'next/head'
+import { Html } from 'next/document'
+import { randomBytes } from 'crypto'
+
+
 
 function MyApp({ Component, pageProps }) {
+  const nonce = randomBytes(128).toString('base64')
+  const csp = `object-src 'none'; base-uri 'none'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https: http: 'nonce-${nonce}' 'strict-dynamic'`
+
   return ( 
-    <Provider store={store}>
-      <Header />
-      <CategoryRoutes />
-      <Component {...pageProps} />
-      <Footer />
-    </Provider>
+    <Html lang='ru'>
+      <Head nonce={nonce}>
+        <meta httpEquiv="Content-Security-Policy" content={csp} />
+      </Head>
+      <body>
+        <Provider store={store}>
+          <Header />
+          <CategoryRoutes />
+          <Component {...pageProps} />
+          <Footer />
+        </Provider>
+      </body>
+    </Html>
     )
 }
 
